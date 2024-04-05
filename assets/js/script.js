ece300723b0a9b00c7a4288d979e25ac12d532df
Dfyerman <fyermandaniel@gmail.com>
2024-04-04 23:29:18 -0700

    started working on javascript to create a new task everytime the button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,27 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
-
-}
+let randomTaskId = self.crypto.randomUUID();
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+  const taskCard = $('<div>')
+    .addClass('card task-card draggable my-3')
+    .attr('data-task-id', task.id);
+  const taskHeader = $('<div>').addClass('card-header h4').text(task.name);
+  const taskBody = $('<div>').addClass('card-body');
+  const taskDescription = $('<p>').addClass('card-text').text(task.type);
+  const taskDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+  const taskDeleteBtn = $('<button>')
+    .addClass('btn btm-danger delete')
+    .text('Delete')
+    .attr('data-project-id', task.id);
+  taskDeleteBtn.on('click', handleDeleteTask);
 
-}
+
+  
+
+};
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
